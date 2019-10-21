ff9ec849049f47e191e4f779be4f51e7211785ad
Vinicius Amélio <vinicius.amelio@zoho.com>
2019-10-21 20:49:11 -0300

    Iniciando repositório

---
+++ 01 - HelloWorld/app.js
@@ -0,0 +1,7 @@
+console.log("Hello World!!")
+
+let name = "Vinicius"
+let idade = 19
+let altura = 1.71
+
+console.log("Olá, meu nome é "+name+", tenho "+idade+" anos e "+altura+"m de altura")
---
+++ 02 - Modulos/app.js
@@ -0,0 +1,9 @@
+var multiply = require("./multiplicar")
+var add = require("./somar")
+var sub = require("./subtrair")
+var divide = require("./dividir")
+
+console.log(multiply(5,2))
+console.log(add(5,10))
+console.log(sub(50,30))
+console.log(divide(5,5))
