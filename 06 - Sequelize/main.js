c1d063687416ed3a69ac8d3de15129bbe462e322
Vinicius Amélio <vinicius.amelio@zoho.com>
2019-10-30 21:40:42 -0300

    Adicionando layout default e configurando main.js

--- 06 - Sequelize/main.js
+++ 06 - Sequelize/main.js
@@ -2,10 +2,17 @@ const express = require("express")
 const handeblars = require("express-handlebars")
 const main = express()
 
-//Template Engine
-    main.engine('handlebars',handeblars({
-        defaultLayout: "main"
-    }))
-    main.set('view-engine','handlebars')
+//Config
+    //Template Engine
+        main.engine('handlebars',handeblars({
+            defaultLayout: "main"
+        }))
+        main.set('view-engine','handlebars')
+    //Database
+        const Sequelize = require('sequelize')
+        const sequelize = new Sequelize('node','root','',{
+            host: "localhost",
+            dialect: "mysql"
+        })
 
 main.listen(8081)
